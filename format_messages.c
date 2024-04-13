d8dab449187e1d21901b6d98e7415c4680841344
Juju <julie.elanor@free.fr>
2024-04-13 16:27:01 +0200

    struct pour les messages de début du serveur

--- format_messages.c
+++ format_messages.c
@@ -1,4 +1,7 @@
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
 /***  Messages du client ***/
 
@@ -17,6 +20,15 @@
 /*** Messages du serveur ***/
 
 /* intégrer et démarer une partie */
+
+typedef struct message_debut_serveur {
+    uint16_t CODEREQ :13;
+    uint16_t ID : 2;
+    uint16_t EQ : 1;
+    uint16_t PORTUDP;
+    uint16_t PORTMDIFF;
+    uint16_t ADRMDIFF[8];
+} message_debut_serveur;
 
 /* déroulement d'une partie */
 
