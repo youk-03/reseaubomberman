5daa950c7fe62dc8df03ea6b18e1924d39bba605
Juju <julie.elanor@free.fr>
2024-04-13 16:10:08 +0200

    fichier pour définir les struct des messages

---
+++ format_messages.c
@@ -0,0 +1,25 @@
+
+
+/***  Messages du client ***/
+
+
+/* intégrer et démarrer une partie */
+
+
+
+/*  déroulement de la partie */
+
+
+/* le tchat */
+
+
+
+/*** Messages du serveur ***/
+
+/* intégrer et démarer une partie */
+
+/* déroulement d'une partie */
+
+/* le tchat */
+
+/* la fin de partie */
